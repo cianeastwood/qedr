df838e17ff9c67095a01b12790a193ebbcc49485
cian.eastwood@gmail.com <s1668298@osiris.inf.ed.ac.uk>
2018-01-20 21:02:28 +0000

    Factorised PCA model.

--- models/pca/main.py
+++ models/pca/main.py
@@ -1,38 +1,33 @@
 import numpy as np
 import os, sys
-from model import encode
+from sklearn.decomposition import PCA
 from lib.eval.regression import normalize
       
 if __name__ == '__main__':
-    # Sys args: [filename, m_name, n_components, gaps, train_fract, data_dir]
-    try:
-        m_name = sys.argv[1]
-    except Exception:
-        m_name = 'pca'
-    print("Using {0} model.".format(m_name.upper()))
+    # Sys args: [filename, n_components, gaps, train_fract, data_dir]
     
     try:
-        n_components = int(sys.argv[2])
+        n_components = int(sys.argv[1])
     except Exception:
         n_components = 6
     print("Using {0} components.".format(n_components))
         
     try:
-        gaps = True if str(sys.argv[3]).strip() == "True" else False
+        gaps = True if str(sys.argv[2]).strip() == "True" else False
     except Exception:
-        gaps = False
+        gaps = True
     print("Using train data with gaps: {0}".format(gaps))
         
     try:
-        train_fract = float(sys.argv[4])
+        train_fract = float(sys.argv[3])
     except Exception:
         train_fract = 0.8
     print("Using {0} as train_fract.".format(train_fract))
     
     try:
-        data_dir = str(sys.argv[5])
+        data_dir = str(sys.argv[4])
     except Exception:
-        data_dir = '../../data'
+        data_dir = '/disk/scratch1/cian/data/'#'../../data'
     print("Loading data from {0}.".format(data_dir))
         
     # Load data
@@ -50,10 +45,14 @@ if __name__ == '__main__':
     data, _, _, _   = normalize(data, m, s)
     
     # Encode
-    results_dict = encode(m_name, n_components, train, data)
-    
-    # Save results
+    rng = np.random.RandomState(123)
+    model = PCA(n_components=n_components, random_state=rng)
+    model.fit(train)
+    data = model.transform(data) # codes, reuse var
+    expl_var = np.array(np.sum(model.explained_variance_ratio_))
+
+    # Save results   
     codes_dir = os.path.join(data_dir, 'codes/')
-    filename = os.path.join(codes_dir, "{0}_{1}_{2}_{3}v_test".format(m_name, gaps, n_components))
-    np.savez_compressed(filename, **results_dict)
+    filename = os.path.join(codes_dir, "pca_{0}_{1}v_new_T".format(gaps, n_components))
+    np.savez_compressed(filename, codes=data, expl_var=expl_var)
     print("{0} values saved to {1}.".format(m_name, filename))
