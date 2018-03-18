c3c49ed83f5b0c1a0ce70d2c6144fa43da788f79
Marcin Malinowski <szyszka45@gmail.com>
2018-03-18 12:18:30 +0000

    fix indentation

--- models/pca/main.py
+++ models/pca/main.py
@@ -3,51 +3,51 @@ import os, sys
 from operator import mul
 from sklearn.decomposition import PCA
 from lib.eval.regression import normalize
-      
+
 if __name__ == '__main__':
     # Sys args: [filename, n_components, gaps, train_fract, data_dir]
-    
+
     try:
         n_components = int(sys.argv[1])
     except Exception:
         n_components = 6
     print("Using {0} components.".format(n_components))
-        
+
     try:
         gaps = True if str(sys.argv[2]).strip() == "True" else False
     except Exception:
         gaps = True
     print("Using train data with gaps: {0}".format(gaps))
-        
+
     try:
         train_fract = float(sys.argv[3])
     except Exception:
         train_fract = 0.8
     print("Using {0} as train_fract.".format(train_fract))
-    
+
     try:
         data_dir = str(sys.argv[4])
     except Exception:
         data_dir = '../../data/'
     print("Loading data from {0}.".format(data_dir))
-        
+
     # Load data
     def load_data():
         data = np.load(os.path.join(data_dir, 'teapots.npz'))['images']
         if len(data.shape) > 2:
-	    shape = reduce(mul, data.shape[1:], 1)
-	    data = data.reshape([-1, shape])
+            shape = reduce(mul, data.shape[1:], 1)
+        data = data.reshape([-1, shape])
         return data
-    
+
     data = load_data()
-	
+
     # Get training data
     n_train = int(len(data) * train_fract)
-    data = data[:n_train] #reuse var to save memory        
+    data = data[:n_train] #reuse var to save memory
     if gaps:
         gap_ids = np.load(os.path.join(data_dir, 'gap_ids.npy'))
         data = np.delete(data, gap_ids, 0)
-    
+
     # Fit model to normalized training data
     data, m, s, fs = normalize(data)
     rng = np.random.RandomState(123)
@@ -60,7 +60,7 @@ if __name__ == '__main__':
     data = model.transform(data) # codes, reuse var
     expl_var = np.array(np.sum(model.explained_variance_ratio_))
 
-    # Save results   
+    # Save results
     codes_dir = os.path.join(data_dir, 'codes/')
     f_name = os.path.join(codes_dir, "pca_{0}_{1}v".format(gaps, n_components))
     np.savez_compressed(f_name, codes=data, expl_var=expl_var)
